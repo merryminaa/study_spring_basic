3e6febc07e6c7bd0cad82e0f3150020f069ee14a
Mina Park <kyminp@gmail.com>
2023-09-30 17:43:28 +0900

    add: 케이스별 스프링 빈 조회
    - 기본, 동일한 타입이 둘 이상인 경우, 상속관계인 경우 빈 조회 test case 작성

---
+++ src/test/java/basic/core/beanFind/ApplicationContextBasicFindTest.java
@@ -0,0 +1,47 @@
+package basic.core.beanFind;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import basic.core.AppConfig;
+import basic.core.member.MemberService;
+import basic.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApplicationContextBasicFindTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 이름으로 조회하기")
+    void findBeanByName() {
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("빈 타입으로 조회하기")
+    void findBeanByType() {
+        MemberService memberService = ac.getBean(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("구체 타입으로 조회하기") // 권장하지는 않음
+    void findBeanByName2() {
+        MemberService memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("빈 이름으로 조회 오류 발생시키기")
+    void findBeanByNameNotExist() {
+        org.junit.jupiter.api.Assertions.assertThrows(NoSuchBeanDefinitionException.class,
+            () -> ac.getBean("xxxxx", MemberService.class));
+    }
+
+}
