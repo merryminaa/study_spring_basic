975f8c7ad4d67fd5747d24d74c645ef771c8a406
Mina Park <kyminp@gmail.com>
2023-09-30 15:14:09 +0900

    fix: AppConfig 리팩토링

--- src/main/java/basic/core/AppConfig.java
+++ src/main/java/basic/core/AppConfig.java
@@ -1,6 +1,8 @@
 package basic.core;
 
+import basic.core.discount.DiscountPolicy;
 import basic.core.discount.FixDiscountPolicy;
+import basic.core.discount.RateDiscountPolicy;
 import basic.core.member.MemberService;
 import basic.core.member.MemberServiceImpl;
 import basic.core.member.MemoryMemberRepository;
@@ -12,11 +14,20 @@ public class AppConfig {
 
     //생성자 주입방식
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository(); //역할과 구현 클래스 분리
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private DiscountPolicy discountPolicy() {
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 
 }
