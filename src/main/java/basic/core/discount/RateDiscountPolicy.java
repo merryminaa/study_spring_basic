e87302bdaf29a1afb3e00c6846c8362c761fccf8
Mina Park <kyminp@gmail.com>
2023-10-22 12:37:36 +0900

    add: annotation 직접 정의 적용

--- src/main/java/basic/core/discount/RateDiscountPolicy.java
+++ src/main/java/basic/core/discount/RateDiscountPolicy.java
@@ -1,10 +1,12 @@
 package basic.core.discount;
 
+import basic.core.annotation.MainDiscountPolicy;
 import basic.core.member.Grade;
 import basic.core.member.Member;
 import org.springframework.stereotype.Component;
 
 @Component
+@MainDiscountPolicy //직접등록한 어노테이션
 public class RateDiscountPolicy implements DiscountPolicy {
 
     private int discountRate = 10; //정률 할인
--- src/main/java/basic/core/order/OrderServiceImpl.java
+++ src/main/java/basic/core/order/OrderServiceImpl.java
@@ -1,5 +1,6 @@
 package basic.core.order;
 
+import basic.core.annotation.MainDiscountPolicy;
 import basic.core.discount.DiscountPolicy;
 import basic.core.discount.FixDiscountPolicy;
 import basic.core.discount.RateDiscountPolicy;
@@ -11,17 +12,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-//    @Autowired
-//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-//        this.memberRepository = memberRepository;
-//        this.discountPolicy = discountPolicy;
-//    }
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
