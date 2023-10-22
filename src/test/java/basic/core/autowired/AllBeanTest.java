1efd4d0eea83dab70bbbce8b9dfef5542d806809
Mina Park <kyminp@gmail.com>
2023-10-22 13:50:14 +0900

    add: 조회한 빈 모두 조회

---
+++ src/test/java/basic/core/autowired/AllBeanTest.java
@@ -0,0 +1,49 @@
+package basic.core.autowired;
+
+import basic.core.AutoAppConfig;
+import basic.core.discount.DiscountPolicy;
+import basic.core.member.Grade;
+import basic.core.member.Member;
+import java.util.List;
+import java.util.Map;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class AllBeanTest {
+
+    @Test
+    void findAllBean() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class, DiscountService.class);
+        DiscountService discountService = ac.getBean(DiscountService.class);
+        Member member = new Member(1L, "userA", Grade.VIP);
+
+        Assertions.assertThat(discountService).isInstanceOf(DiscountService.class);
+        int dcPrice1 = discountService.discount(member, 10000, "fixDiscountPolicy");
+        Assertions.assertThat(dcPrice1).isEqualTo(1000);
+
+        int dcPrice2 = discountService.discount(member, 20000, "rateDiscountPolicy");
+        Assertions.assertThat(dcPrice2).isEqualTo(2000);
+    }
+
+    static class DiscountService {
+        private final Map<String, DiscountPolicy> policyMap;
+        private final List<DiscountPolicy> policyList;
+
+        @Autowired
+        public DiscountService(Map<String, DiscountPolicy> policyMap, List<DiscountPolicy> policyList) {
+            this.policyMap = policyMap;
+            this.policyList = policyList;
+            System.out.println("policyMap = " + policyMap);
+            System.out.println("policyList = " + policyList);
+        }
+
+        public int discount(Member member, int price, String discountCode) {
+            DiscountPolicy discountPolicy = policyMap.get(discountCode);
+            return discountPolicy.discount(member, price);
+        }
+    }
+
+}
